bb4be5c1780e99f3f919c78553f1cacbaa0b8d0d
Ole Walberg <greblavelo@gmail.com>
2021-10-12 21:05:43 +0200

    mobile version is close to done

---
+++ js/burger.js
@@ -0,0 +1,8 @@
+const burgerToggle = document.querySelector('.burgerMenu');
+const navMenu = document.querySelector('.navMenu');
+const nav = document.querySelector('nav');
+
+burgerToggle.addEventListener('click', () => {
+    nav.classList.toggle('navRow');
+    navMenu.classList.toggle('toggleDisplay')
+})
